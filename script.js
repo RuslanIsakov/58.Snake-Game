5025bffa84661ff8f6257f399f21442267e21f0a
Ruslan Rustanovich ISAKOV <46198640+RuslanIsakov@users.noreply.github.com>
2020-06-17 10:31:59 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+﻿window.onload = function() {
+  document.addEventListener('keydown', changeDirection);
+  setInterval(loop, 1000/60); // 60 FPS
+}
 
+var
+  canv 				= document.getElementById('mc'), // canvas
